1ec2cc9a575b5128ddf1b7db9abd7f3639d56cf6
michael-steele-nox <48181422+michael-steele-nox@users.noreply.github.com>
2021-09-01 21:42:42 +0300

    Implement phone mask. Added navbar panel for responsive version
    Implement phone mask. Added navbar panel for responsive version

--- js/main.js
+++ js/main.js
@@ -1,21 +1,38 @@
 $(document).ready(function () {
 //	console.log("Сайт готов к манипуляции");
 	
+	
+	
 	var form = $(".form-popup");
 	var formCloseButton = $(".button-close");
 	var formPopupButton = $(".search-button");
 	
+	var navbarPanel = $(".navbar-panel");
+	var navbarMenuButton = $(".navbar-menu__button");
+	var navbarCloseButton = $(".navbar-menu__close-button");
+	
+	
 	formCloseButton.on('click', toggleForm);
 	formPopupButton.on('click', toggleForm);
-
-
+	navbarMenuButton.on('click', toggleNavbar);
+//	navbarPanel.on('click', toggleNavbar);
+	navbarCloseButton.on('click', toggleNavbar);
+	
 	function toggleForm() {
 		form.toggleClass("is-open");
 	};
 	
+	function toggleNavbar() {
+		navbarPanel.toggleClass("navbar-open");
+	};
+	
+	
+	// implement phone mask
+	
 	$("#phone").mask("+375(99)999-99-99");
 	
 	
+
 	
 	
 	
